c3996ba6c9c02393120d70a8c0b94d10794bf655
Marius CA <marius@example.com>
2023-02-09 13:01:50 +0200

    html

--- js/html.js
+++ js/html.js
@@ -1,2 +1,21 @@
 'use strict';
 console.log('html.js file was loaded');
+
+// parasyti funkcija kuri ijungia darkMode
+// body uzdeti klase darkMode
+
+// sideEffect fn
+function setDarkModeOn() {
+  document.body.className = 'darkMode';
+  console.log('document.body.className ===', document.body.className);
+}
+function setLightModeOn() {
+  document.body.className = '';
+  console.log('document.body.className ===', document.body.className);
+}
+// setDarkModeOn();
+
+// pr7. Sukurti funkcija toggleMode()
+// jei yra siuo metu dark mode ijungtas tai isjungia
+// jei yra light mode ijungtas tai isjungia
+function toggleMode() {}
---
+++ readme.md
@@ -0,0 +1,4 @@
+# practice
+
+1. Sukurti funkcija minutesToSeconds(minutes). Fn atspausdina ir grazina kiek sekundziu gautusi pavertum minutes skliausteliuose
+   (getSeconds(5) - 300sek)
