0580e6c66176eb2a9cfa630442c5d4936c3a7178
Marius CA <marius@example.com>
2023-02-09 10:59:51 +0200

    default args

--- js/parameters.js
+++ js/parameters.js
@@ -2,7 +2,7 @@
 console.log('parameters.js file was loaded');
 
 // global variables
-let n1 = 51;
+let n1; /// undefined
 let n2 = 10;
 // debugger;
 function add(sk1, sk2) {
@@ -10,14 +10,39 @@ function add(sk1, sk2) {
   console.log(`${sk1} + ${sk2} = ${sum}`);
 }
 
-add(5, 10);
-add(51, 10);
-add(71, 50);
+// add(5, 10);
+// add(51, 10);
+// add(71, 50);
 
 function sp(message) {
   console.log('spausdinama reiksme');
   console.log(message);
+  // console.log('arguments ===', arguments);
 }
 
-sp(n2);
-sp('Laba diena');
+// sp(n2);
+// sp('Laba diena');
+
+// parasyti fn printAvg3()
+// funkcija gauna tris argumentus ir isveda ju bendra suma ir vidurki i konsole
+// panaudoti su bent 3 skirtingom reikmem
+
+function printAvg3(sk1, sk2, sk3) {
+  console.log('sk1, sk2, sk3 ===', sk1, sk2, sk3);
+  let total = sk1 + sk2 + sk3;
+  console.log('total ===', total);
+  let avg = total / 3;
+  console.log(`${total} / 3 === ${avg.toFixed(2)}`);
+}
+
+// printAvg3(1, 2, 3);
+// printAvg3(4, 5, 6);
+
+// function argument defaults
+
+function sayHi(user = 'Mr or Ms') {
+  console.log(`hello ${user}, how is your day?`);
+}
+sayHi('Nick');
+sayHi('Jane');
+sayHi();
